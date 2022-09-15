17d10cce4de354c9877b1b8fbf4c9dbc6fb91e9c
freetimezp <kraftukrnet@ukr.net>
2022-09-15 22:11:28 +0300

    09. js-sport-01. Calculate block. Calculate func.

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,49 @@ window.addEventListener('scroll', scrollHeader);
 
 
 /*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form');
+const calculateCm = document.getElementById('calculate-cm');
+const calculateKg = document.getElementById('calculate-kg');
+const calculateMessage = document.getElementById('calculate-message');
+
+const calculateBmi = (e) => {
+    e.preventDefault();
+
+    if(calculateCm.value === '' || calculateKg.value === '') {
+        calculateMessage.classList.remove('color-green');
+        calculateMessage.classList.add('color-red');
+
+        calculateMessage.textContent = 'Fill the Height and Weight!';
+        setTimeout(() => {
+            calculateMessage.textContent = '';
+        }, 3000);
+    }else{
+        const cm = calculateCm.value / 100;
+        const kg = calculateKg.value;
+        const bmi = Math.round(kg / (cm * cm));
+
+        if(bmi < 18.5) {
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny.`;
+        }else if(bmi < 25) {
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy.`;
+        }else{
+            calculateMessage.classList.add('color-green');
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight.`;
+        }
+
+        calculateCm.value = '';
+        calculateKg.value = '';
+
+        setTimeout(() => {
+            calculateMessage.textContent = '';
+        }, 4000);
+    }
+}
+
+calculateForm.addEventListener('submit', calculateBmi);
+
 
 
 /*=============== EMAIL JS ===============*/
