72486113075b41c9c4c9f3a8deb216c5907f9a94
freetimezp <kraftukrnet@ukr.net>
2022-09-16 13:38:47 +0300

    13. js-sport-01. Custom css on scroll and show, hide scroll button.

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,7 +56,12 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUP = () => {
+    const scrollup = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll') : scrollup.classList.remove('show-scroll');
+}
 
+window.addEventListener('scroll', scrollUP);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
